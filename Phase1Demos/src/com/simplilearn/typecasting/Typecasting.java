f3e8035209f01e59712cbe6f8a6ffb4b7fe05934
asp <asp@asp-virtual-machine>
2020-07-28 13:59:07 +0530

    Added Comments

--- Phase1Demos/src/com/simplilearn/typecasting/Typecasting.java
+++ Phase1Demos/src/com/simplilearn/typecasting/Typecasting.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Typecasting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//User Input
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter an integer: ");
 		int numInt = scan.nextInt();
--- Phase1Demos/src/com/simplilearn/typecasting/Typecasting2.java
+++ Phase1Demos/src/com/simplilearn/typecasting/Typecasting2.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 public class Typecasting2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//User Input
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter any number: ");
 		String userInput = scan.nextLine();
+		
+		//Non primary to primary data type conversion
 		int cInt = Integer.parseInt(userInput);
 		byte cByte = Byte.parseByte(userInput);
 		float cFloat = Float.parseFloat(userInput);
 		double cDouble = Double.parseDouble(userInput);
+		
+		//Output
 		System.out.println("Note: If the entered value is out of the range -128 to 127, byte value is not accepted and an exception is caught");
 		System.out.println("Integer value - "+cInt);
 		System.out.println("Byte value - "+cByte);
