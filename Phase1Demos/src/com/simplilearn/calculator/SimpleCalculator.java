d8acd40c5613550e9c02d1f0f8c2001ac3fafc48
asp <asp@asp-virtual-machine>
2020-07-29 21:13:33 +0530

    Simple calculator first commit

---
+++ Phase1Demos/src/com/simplilearn/calculator/SimpleCalculator.java
@@ -0,0 +1,68 @@
+package com.simplilearn.calculator;
+import java.util.*;
+
+public class SimpleCalculator {
+	
+	public static int add(int x, int y) {
+		int result = x+y;
+		System.out.println("Sum of "+x+" and "+y+" is :");
+		return result;
+	}
+	
+	public static int subtract(int x, int y) {
+		int result = x-y;
+		System.out.println("Difference between "+x+" and "+y+" is :");
+		return result;
+	}
+	
+	public static double multiply(double x,double y) {
+		double result = x*y;
+		System.out.println("Product of "+x+" and "+y+" is :");
+		return result;
+	}
+	
+	//divide method
+	public static double divide(double x, double y) {
+		double result = x/y;
+		System.out.println("Sum of "+x+" and "+y+" is :");
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+		int num1, num2;
+		System.out.println("--------Simple Calculator--------");
+		System.out.println("1: Addition");
+		System.out.println("2: Subtraction");
+		System.out.println("3: Multiplication");
+		System.out.println("4: Division");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter two numbers :");
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+		
+		System.out.println("Enter your choice to perform specific operation");
+		int result = scan.nextInt();
+		
+		switch(result) {
+		case 1:
+			System.out.println(add(num1, num2));
+			break;
+		case 2:
+			System.out.println(subtract(num1, num2));
+			break;
+		case 3:
+			System.out.println(multiply(num1, num2));
+			break;
+		case 4:
+			System.out.println(divide(num1, num2));
+			break;
+			default:
+				System.out.println("Invalid entry, please select valid option");
+
+		}
+		
+	}
+
+}
